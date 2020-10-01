0ee1544703003d56984db57644cd9ef5341ef75d
Manuel Castro <manuelcastro@mbp-de-admin.ad.heb.com.mx>
2020-10-01 17:48:39 -0500

    primera version

---
+++ app/code/Seis10/Csp/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright: Copyright © 2020 Seis10 Services. All rights reserved.
+ * @author   : Laura Trejo <laura@seis10.com>
+ * @site : https://seis10.com/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Seis10_Csp',
+    __DIR__
+);
